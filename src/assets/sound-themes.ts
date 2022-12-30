ce214247857a89cc5afe6355b655269c2fa7ec70
Louis MONESTIER <louis.monestier@hotmail.fr>
2022-12-30 22:56:47 +0100

    add themes

--- src/assets/sound-themes.ts
+++ src/assets/sound-themes.ts
@@ -17,13 +17,31 @@ const f = <T extends Record<string, number>>(s: T) =>
   ) as Record<keyof T, SoundFamilyDef>;
 
 export const soundThemes = f({
-  mario: 3,
-  zelda: 3,
-  yoshi: 3,
+  accordeon: 3,
+  banjos: 3,
   bowser: 3,
+  cornemuse: 3,
+  cristal_baschet: 3,
+  didgeridoo: 3,
+  flute: 3,
+  gimbarde: 3,
+  gong: 3,
+  guitare: 3,
+  harmonica: 3,
+  harpe: 3,
+  harry_potter: 3,
+  mario: 3,
+  orgue: 3,
   pokemon: 3,
-  tetris: 3,
+  saxophone: 3,
+  seigneur_des_anneaux: 3,
   sonic: 3,
+  tetris: 3,
+  triangle: 3,
+  udu: 3,
+  xylophone: 3,
+  yoshi: 3,
+  zelda: 3,
 });
 
 export type SoundThemeKey = keyof typeof soundThemes;
